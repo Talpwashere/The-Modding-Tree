5146ccb7428238e1c983ea658c77b273936e42bb
I'm bad at titles <CJRod03@outlook.com>
2020-12-02 22:04:15 -0800

    RNG
    doesn't work yet lol

--- js/layers.js
+++ js/layers.js
@@ -1,14 +1,14 @@
-addLayer("p", {
-    name: "prestige", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
+addLayer("rnp", {
+    name: "RNPrestige", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "RNP", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
 		points: new Decimal(0),
     }},
-    color: "#4BDC13",
-    requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "prestige points", // Name of prestige currency
+    color: "#850000",
+    requires: new Decimal(1), // Can be a function that takes requirement increases into account
+    resource: "RNPrestige points", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
@@ -22,7 +22,39 @@ addLayer("p", {
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
-        {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+        {key: "r", description: "R: Reset for RNP points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
-    layerShown(){return true}
+    layerShown(){return true},
+    upgrades: {
+        rows: 2,
+        cols: 3,
+        11: {
+            title: "The Beginning",
+            description: "Add 1/10th of a Point every second.",
+            cost: new Decimal(1),
+            unlocked() { return player[this.layer].unlocked }
+        },
+        12: {
+            title: "RNG",
+            description: "Multiplies Point Value between 0 and 10x",
+            cost: new Decimal(1),
+            unlocked() { return (hasUpgrade(this.layer, 11))},
+            update(diff) {
+                new decimal(60)
+            },
+            effect() {
+                let ret = (Math.random()*11)
+                return ret
+            }
+        },
+    }
+}),
+addlayer("time", {
+    startData() { return {
+        unlocked: true,
+    }},
+    name: "time",
+    symbol: "t",
+    color: "white",
+    position: "side",
 })
--- js/mod.js
+++ js/mod.js
@@ -1,20 +1,20 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	name: "RNG Tree",
+	id: "RNGmod",
+	author: "me",
 	pointsName: "points",
 	discordName: "",
 	discordLink: "",
 	changelogLink: "https://github.com/Acamaeda/The-Modding-Tree/blob/master/changelog.md",
-	initialStartPoints: new Decimal (10), // Used for hard resets and new players
+	initialStartPoints: new Decimal (1), // Used for hard resets and new players
 	
 	offlineLimit: 1,  // In hours
 }
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.0",
-	name: "Literally nothing",
+	num: "0.1",
+	name: "RNP",
 }
 
 // If you add new functions anywhere inside of a layer, and those functions have an effect when called, add them here.
@@ -27,7 +27,7 @@ function getStartPoints(){
 
 // Determines if it should show points/sec
 function canGenPoints(){
-	return true
+	return hasUpgrade("rnp", 11)
 }
 
 // Calculate points/sec!
@@ -35,7 +35,8 @@ function getPointGen() {
 	if(!canGenPoints())
 		return new Decimal(0)
 
-	let gain = new Decimal(1)
+	let gain = new Decimal(0.1)
+	if (hasUpgrade("rnp", 12)) gain = gain.times(upgradeEffect("rnp",12))
 	return gain
 }
 
@@ -58,7 +59,7 @@ function isEndgame() {
 
 // You can change this if you have things that can be messed up by long tick lengths
 function maxTickLength() {
-	return(3600) // Default is 1 hour which is just arbitrarily large
+	return(60) // Default is 1 hour which is just arbitrarily large
 }
 
 // Use this if you need to undo inflation from an older version. If the version is older than the version that fixed the issue,
